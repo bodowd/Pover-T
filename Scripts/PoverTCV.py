19725fa561685323678e1f228f107680c102b038
Bing O'Dowd <Bing@Bings-MacBook-Air.local>
2018-01-10 21:32:06 -0600

    proper CV, and added new options for Tools

--- Scripts/PoverTCV.py
+++ Scripts/PoverTCV.py
@@ -11,76 +11,115 @@ Treat each country seperately since each country will use a different model.
 
 
 """
+import sys
+sys.path.append("/Users/Bing/Documents/DS/DrivenData/Pover-T/Scripts/") # need to add path to the parent folder where CV.py is
 
 import pandas as pd
 import numpy as np
-import PoverTHelperTools
+from PoverTHelperTools import *
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from sklearn.model_selection import StratifiedKFold
+from sklearn.utils import resample
+from sklearn.model_selection import StratifiedKFold, KFold
 from sklearn.metrics import log_loss
 
+from sklearn.preprocessing import LabelEncoder
+from imblearn.over_sampling import SMOTE
 
-def StratifiedKF(df, n_splits, model,categorical = 'label_encoder',enforce_cols = None, fillmean = None, verbose = False):
+def resample_data(df, how = 'down'):
     """
-    df: train dataframe with `poor` and `country` still in columns. will be dropped in this function, and `poor` will be used to define y in this func
-    model: classifier with sklearn-like API 
-
-    """
-    avg_train_logloss = []
-    avg_valid_logloss = []
-    y_valid_preds = np.array([])
-
-    X = df.drop(['poor', 'country'], axis = 1)
-    y = df['poor'].values.astype(int)
-
-    skf = StratifiedKFold(n_splits=n_splits, random_state = 25)
-    for i, (train_index, val_index) in enumerate(skf.split(X,y)):
-        X_train, X_val = X.iloc[train_index], X.iloc[val_index]
-        y_train, y_val = y[train_index], y[val_index]
-        
-        # pre process data here after the split to avoid information leak
-        print('Preproc train')
-        X_train = PoverTHelperTools.pre_process_data(X_train, enforce_cols = enforce_cols, categorical = categorical, fillmean = fillmean)
-        print('Preproc valid')
-        X_val = PoverTHelperTools.pre_process_data(X_val, enforce_cols = enforce_cols, categorical = categorical, fillmean = fillmean)
-        
-        # models here
-        print('*****\nTraining model on Fold {}'.format(i))
-        model.fit(X_train, y_train)
-
-        preds = model.predict_proba(X_val)
-        # print(len(preds))
-        # print(preds)
-        valid_logloss = log_loss(y_val, preds[:,1])
-        train_logloss = log_loss(y_train, model.predict_proba(X_train)[:,1])
-        avg_train_logloss.append(train_logloss)
-        avg_valid_logloss.append(valid_logloss)
-        
-        y_valid_preds = np.concatenate((y_valid_preds,preds[:,1]))
-
-        if verbose:
-            print('\nTrain Log Loss for Fold {}: {}'.format(i, train_logloss))
-            print('Validation Log Loss for Fold {}: {}\n'.format(i, valid_logloss))
-
-    print('******\nAverage Train Log Loss: {}'.format(np.mean(avg_train_logloss)))
-    print('Average Validation Log Loss: {}\n'.format(np.mean(avg_valid_logloss)))
+    Upsample or Downsample data to balance classes for CV
     
-    return y, y_valid_preds
+    """
+    df_majority = df[df['poor'] == 0]
+    df_minority = df[df['poor'] == 1]
 
-def mean_log_loss(y_true_a, y_pred_a,
-                  y_true_b, y_pred_b,
-                  y_true_c, y_pred_c):
-    # average of each countries log loss
-    a_logloss = log_loss(y_true_a, y_pred_a)
-    b_logloss = log_loss(y_true_b, y_pred_b)
-    c_logloss = log_loss(y_true_c, y_pred_c)
-    # average of those 3 countries
-    # return np.sum([a_logloss, b_logloss, c_logloss])/3
-    print('Weighted average')
-    return np.sum([a_logloss*len(y_true_a), b_logloss*len(y_true_b), c_logloss*len(y_true_c)])/(np.sum([len(y_true_a), len(y_true_b), len(y_true_c)]))
+    if how == 'down':
+        print('Downsampling data...')
+        df_majority_downsampled = resample(df_majority,
+                                           replace = False, # sample without replacement
+                                           n_samples = df_minority.shape[0], # to match minority class
+                                           random_state = 144
+                                           )
+        df_resampled = pd.concat([df_majority_downsampled, df_minority])
+    elif how == 'up':
+        print('Upsampling data...')
+        df_minority_upsampled = resample(df_minority,
+                                          replace = True, #sample with replacement
+                                          n_samples = df_majority.shape[0], # to match majority class amount
+                                          random_state = 144)
+        df_resampled = pd.concat([df_majority, df_minority_upsampled])
+
+    return df_resampled
+
+def run_CV(X,y,model,func, n_splits = 3, how = 'up', categorical = 'label_encoder'):
+    """
+    Proper CV for unbalanced target
+
+    
+    X: training set with poor still in the column. Drop country
+    y: target (`poor`)
+    model: classifier to use
+    func: feature engineering function i.e. feature_eng_a, etc
+    how: upsample or downsample
+    categorical: how to treat categorical features
+    
+    Returns: a list of logloss values of each fold
+    """
+    logloss = []
+    skf = StratifiedKFold(n_splits = n_splits, random_state = 144)
+    for i, (train_idx, val_idx) in enumerate(skf.split(X,y)):
+        X_train, X_val = X.iloc[train_idx], X.iloc[val_idx]
+        y_train, y_val = y[train_idx], y[val_idx]
+
+        # # SMOTE
+        # X_train = X_train.drop('poor', axis = 1) # drop target
+        # cat_columns = X_train.select_dtypes(['object']).columns
+        # X_train[cat_columns] = X_train[cat_columns].apply(LabelEncoder().fit_transform)
+        # orig_cols = X_train.columns # SMOTE will return a numpy array. Store the column names here to recreate the dataframe for feature engineering/transforms below
+        # X_train, y_train = SMOTE().fit_sample(X_train,y_train)
+        # # recreate dataframe
+        # X_train = pd.DataFrame(X_train, columns = orig_cols)
+
+        if how:
+            # resample to balance data
+            X_resampled = resample_data(X_train, how = how)
+            # store the targets now that they are balanced
+            y_train = X_resampled['poor']
+            # drop target from train
+            X_train = X_resampled.drop('poor', axis = 1)
+        ####### feature engineering goes blow this comment:
+       
+        func(X_train)
+       
+        ###### end feature eng
+        X_train = pre_process_data(X_train, normalize_num='standardize', categorical = categorical)
+        assert X_train.shape[0] == y_train.shape[0]
+
+        model.fit(X_train, y_train)
+        # standardize X_val to predict
+        X_val = pre_process_data(X_val,normalize_num= 'standardize', enforce_cols=X_train.columns, categorical = categorical)
+        preds = model.predict_proba(X_val)
+        
+        logloss.append(log_loss(y_val, preds[:,1]))
+    
+    return logloss
+
+def mean_log_loss(a_logloss, b_logloss, c_logloss):
+    """
+    a_logloss, b_logloss, c_logloss: lists of logloss from different folds
+
+    Returns: scalar. mean log loss value
+
+    """
+    # hard code weights because size of test set isn't going to change and this way don't need to load test sets just to get shapes
+    a_weight = 4041/8832 # hhold_a_test.shape[0]/total number of test samples 
+    b_weight = 1604/8832
+    c_weight = 3187/8832
+    return np.average([np.average(a_logloss), np.average(b_logloss), np.average(c_logloss)], weights = [a_weight, b_weight, c_weight])
+
 
 def print_confusion_matrix(confusion_matrix, class_names, figsize = (6,5), fontsize=14, ax = None):
     """Prints a confusion matrix, as returned by sklearn.metrics.confusion_matrix, as a heatmap. Copied from shaypal5/confusion_matrix_pretty_print.py
--- Scripts/PoverTHelperTools.py
+++ Scripts/PoverTHelperTools.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.metrics import log_loss
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
 """Helper functions to load data"""
 def load_hhold_train():
@@ -38,31 +38,83 @@ def standardize(df):
     numeric = df.select_dtypes(include=['int64', 'float64'])
     
     # subtract mean and divide by std
-    df[numeric.columns] = (numeric- numeric.mean()) / numeric.std()
-    
+    df[numeric.columns] = (numeric- numeric.mean()) / numeric.std() 
+    return df
+def min_max_scaler(df):
+    print('Min Max scaling...')
+    numeric = df.select_dtypes(include=['int64', 'float64'])
+    df[numeric.columns] =  MinMaxScaler().fit_transform(df[numeric.columns])
     return df
 
-def keep_important_feats(X, cols):
+def add_noise(series, noise_level):
+    return series * (1 + noise_level * np.random.randn(len(series)))
 
-    X = X[cols]
-
-    return X
+def target_encode(trn_series=None, 
+                  tst_series=None, 
+                  target=None, 
+                  min_samples_leaf=1, 
+                  smoothing=1,
+                  noise_level=0):
+    """
+    Copied from a kernel by olivier on kaggle...
     
+    Smoothing is computed like in the following paper by Daniele Micci-Barreca
+    https://kaggle2.blob.core.windows.net/forum-message-attachments/225952/7441/high%20cardinality%20categoricals.pdf
+    trn_series : training categorical feature as a pd.Series
+    tst_series : test categorical feature as a pd.Series
+    target : target data as a pd.Series
+    min_samples_leaf (int) : minimum samples to take category average into account
+    smoothing (int) : smoothing effect to balance categorical average vs prior  
+    """ 
+    assert len(trn_series) == len(target)
 
-def pre_process_data(df, enforce_cols = None, fillmean=None, categorical = 'label_encoder'):
+    temp = pd.concat([trn_series, target], axis=1)
+    # Compute target mean 
+    averages = temp.groupby(by=trn_series.name)[target.name].agg(["mean", "count"])
+    # Compute smoothing
+    smoothing = 1 / (1 + np.exp(-(averages["count"] - min_samples_leaf) / smoothing))
+    # Apply average function to all target data
+    prior = target.mean()
+    # The bigger the count the less full_avg is taken into account
+    averages[target.name] = prior * (1 - smoothing) + averages["mean"] * smoothing
+    averages.drop(["mean", "count"], axis=1, inplace=True)
+    # Apply averages to trn and tst series
+    ft_trn_series = pd.merge(
+        trn_series.to_frame(trn_series.name),
+        averages.reset_index().rename(columns={'index': target.name, target.name: 'average'}),
+        on=trn_series.name,
+        how='left')['average'].rename(trn_series.name + '_mean').fillna(prior)
+    # pd.merge does not keep the index so restore it
+    ft_trn_series.index = trn_series.index 
+
+    return add_noise(ft_trn_series, noise_level)
+
+def pre_process_data(df, normalize_num = 'standardize', enforce_cols = None, fillmean=None, categorical = 'label_encoder', target = None):
     """
     Standardize the numeric columns and one hot encode the categorical columns
     """
-    
-    print('Input shape:\t{}'.format(df.shape))
-    
-    df = standardize(df)
-    print('After standardization {}'.format(df.shape))
 
+    # First enforce columns
+    # match test set and training set columns
+    if enforce_cols is not None:
+        to_drop = np.setdiff1d(df.columns, enforce_cols)
+        to_add = np.setdiff1d(enforce_cols, df.columns)
+
+        df.drop(to_drop, axis=1, inplace=True)
+        df = df.assign(**{c: 0 for c in to_add})
+    
+    # print('Input shape:\t{}'.format(df.shape))
+    if normalize_num == 'standardize': 
+        df = standardize(df)
+    elif normalize_num == 'min_max':
+        df = min_max_scaler(df)
+    # print('After standardization {}'.format(df.shape))
+
+    # get categorical columns
+    cat_columns = df.select_dtypes(['object']).columns
     # label binarizer
     if categorical == 'label_encoder':
         print('Label encoding categoricals')
-        cat_columns = df.select_dtypes(['object']).columns
         df[cat_columns] = df[cat_columns].apply(LabelEncoder().fit_transform)
         # for col in cat_columns:
             # le = LabelEncoder()
@@ -73,15 +125,15 @@ def pre_process_data(df, enforce_cols = None, fillmean=None, categorical = 'labe
         # get dummies for categoricals--one hot encoder
         df = pd.get_dummies(df)
         print('After converting categoricals:\t{}'.format(df.shape))
-        df.info()
+        # df.info()
     
-    # match test set and training set columns
-    if enforce_cols is not None:
-        to_drop = np.setdiff1d(df.columns, enforce_cols)
-        to_add = np.setdiff1d(enforce_cols, df.columns)
-
-        df.drop(to_drop, axis=1, inplace=True)
-        df = df.assign(**{c: 0 for c in to_add})
+    # elif categorical == 'target':
+        # for col in cat_columns:
+            # df[col] = target_encode(df[col],
+                           # target = target,
+                           # min_samples_leaf=100,
+                           # smoothing = 10,
+                           # noise_level = 0.01)
     
     if fillmean == 'mean':
         print('Filling NaN with mean')
@@ -100,7 +152,6 @@ def pre_process_data(df, enforce_cols = None, fillmean=None, categorical = 'labe
 
 def make_country_sub(preds, test_feat, country):
 
-    
     # just get probabilities for p = 1 with preds[:,1]
     country_sub = pd.DataFrame(data = preds[:,1],
                               columns = ['poor'],
